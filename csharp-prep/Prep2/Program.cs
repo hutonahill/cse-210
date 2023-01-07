6306dae6c3822204bc753d1140e60655636e2e4c
hutonahill <35947141+hutonahill@users.noreply.github.com>
2023-01-07 14:40:52 -0700

    committed Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // get grade percentage form the user
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentStr = Console.ReadLine();
+
+        //convert the string to a float
+        float gradePercent = float.Parse(gradePercentStr);
+        
+        string letter = "";
+        string passFail = "";
+
+        if (gradePercent >= 90){
+            letter = "A";
+            passFail = "Pass";
+        }
+
+        else if (gradePercent >= 80){
+           letter = ("B");
+            passFail = ("Pass");
+        }
+
+        else if (gradePercent >= 70){
+            letter = ("C");
+            passFail = "Pass";
+        }
+
+        else if (gradePercent >= 60){
+            letter = ("D");
+            passFail = "Fail";
+        }
+
+        else if (gradePercent < 60){
+            letter = ("F");
+            passFail = ("Fail");
+        }
+
+        Console.WriteLine(letter);
+        Console.WriteLine(passFail);
+
+
     }
 }
