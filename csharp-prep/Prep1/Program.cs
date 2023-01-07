95e4c579fec54a2d7f8a7db46c1373823abae413
hutonahill <35947141+hutonahill@users.noreply.github.com>
2023-01-07 14:23:37 -0700

    completed prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        // get first name
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+        
+        // get last name
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // display result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
