39c9b39abb76436de4d0ffb425dda749823b20a4
hutonahill <35947141+hutonahill@users.noreply.github.com>
2023-02-20 02:59:58 -0700

    made major porgress on the 9-point algorithum

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,11 +4,12 @@ public class Program{
 	public static void Main(){	
 		string userInput = input("test Command Parceing: ");
 
-	
-        List<string> output = ExtractCommand(userInput);
-        Console.WriteLine(output[0]); // Output: "Hello"
-        Console.WriteLine(output[1]); // Output: "world, how are you?"
 
+        string output = ExtractParameter(userInput);
+        
+        if (output != null){
+            print(output);
+        }
         
 
 	}
@@ -33,29 +34,27 @@ public class Program{
     }
 	
     /// <summary>
-    /// extracts peramiters from a string.
-    /// This code snippet was written with the help of ChatGPT.
+    /// Extracts parameter from a string.
     /// </summary>
-    /// <param name="input">a string</param>
-    /// <returns>A list of peramiters (words surounded with perenthisies, like this :) )</returns>
-    public static List<string> ExtractParamiters(string input){
-        
+    /// <param name="input">A string</param>
+    /// <returns>A parameter (words surrounded with parentheses)</returns>
+    /// <author>This code snippet was written with the help of ChatGPT.</author>
+    public static string ExtractParameter(string input){
         int openParentheses = 0;
         int closeParentheses = 0;
-        List<string> results = new List<string>();
+        string result = "";
 
-        // loop though every charicter in the input
+        // loop through every character in the input
         for (int i = 0; i < input.Length; i++){
-            
-            //if the current char is '('
+
+            // if the current char is '('
             if (input[i] == '('){
 
-                // If a nested parenthesis is detected, 
+                // If a nested parenthesis is detected,
                 // inform the user and return null
-                if (openParentheses > 0) {
-                    
+                if (openParentheses > 0){
                     print(
-                        "The command you input includes nested opening parentheses. " + 
+                        "The command you input includes nested opening parentheses. " +
                         "Please try again."
                     );
                     return null;
@@ -68,13 +67,11 @@ public class Program{
             // if the current char is ')'
             else if (input[i] == ')'){
 
-                
-
-                // If a nested ')' is detected, 
+                // If a nested ')' is detected,
                 // inform the user and return null
-                if (closeParentheses > 0) {
+                if (closeParentheses > 0){
                     print(
-                        "The command you input includes nested closing parentheses. " + 
+                        "The command you input includes nested closing parentheses.  " +
                         "Please try again."
                     );
                     return null;
@@ -82,10 +79,19 @@ public class Program{
                 // if not, add the detected ')' to the count
                 else{
                     closeParentheses++;
+
+                    // make sure tehre arnt multiple perams
+                    if (closeParentheses == 2){
+                        print(
+                            "the command you input includes two peramiters attached to one flag.  " + 
+                            "Please try again."
+                        );
+                        return null;
+                    }
                 }
 
-                // if the number of '(' and ')' are the same, add what 
-                // between them to the output list.
+                // if the number of '(' and ')' are the same, add what
+                // between them to the output string.
                 if (closeParentheses == openParentheses){
                     
                     // add one to the starting index to not include the '('
@@ -94,22 +100,15 @@ public class Program{
                     // subtract 1 form the length so the ')' is not included
                     int length = i - startIndex;
 
-                    // extract the enclose string
-                    string result = input.Substring(startIndex, length);
-
-                    //append the string to the output list
-                    results.Add(result);
-
-                    // remove one form the count of closed and open
-                    closeParentheses --;
-                    openParentheses --;
+                    // extract the enclosed string
+                    result = input.Substring(startIndex, length);
                 }
 
                 // if there are more ')' than '(' inform the user and return null
                 else if (closeParentheses > openParentheses){
                     print(
-                        "The command you input has a closing parentheses " + 
-                        "that comes before an opening parentheses. " + 
+                        "The command you input has a closing parentheses " +
+                        "that comes before an opening parentheses. " +
                         "Please try again."
                     );
                     return null;
@@ -117,25 +116,26 @@ public class Program{
             }
         }
 
-        // make sure all parentheses are paired properly.
-        if (openParentheses != closeParentheses){
+        // If there are any open or close parentheses remaining, inform the user and return null
+        if (openParentheses > 0 || closeParentheses > 0){
             print(
-                "The command you input includes unmatched parentheses." + 
+                "The command you input has an unclosed parentheses. " +
                 "Please try again."
             );
             return null;
         }
 
-        return results;
+        // Return the extracted parameter
+        return result;
     }
 
 
     /// <summary>
     /// extract the command from a string.
-    /// This code snippet was written with the help of ChatGPT.
     /// </summary>
     /// <param name="input">a string</param>
     /// <returns>a list where the first index is the command and the second is the rest of the string</returns>
+    /// <author>This code snippet was written with the help of ChatGPT.</author>
     public static List<string> ExtractCommand(string input){
         List<string> output = new List<string>();
         int firstSpace = input.IndexOf(' ');
@@ -152,36 +152,109 @@ public class Program{
     }
 
 
+
     /// <summary>
-    /// extract flags from a string
-    /// This code snippet was written with the help of ChatGPT.
+    /// Extracts a flag and parameter as a single string from a given input string.
+    /// A flag is defined as a word that starts with the '-' character. 
+    /// The parameter is at the end of the flag and is surrounded by parentheses. 
     /// </summary>
-    /// <param name="input">a string</param>
-    /// <returns>a list of flags (paramiters removed)</returns>
-    public static List<string> ExtractFlags(string input){
+    /// <param name="input">The input string to extract flags and parameters from</param>
+    /// <returns>A List of strings containing the flags and their corresponding parameters (if any)</returns>
+    /// <author>This code snippet was written with the help of ChatGPT.</author>
+    public static List<string> ExtractFlagsAndParameters(string input){
         List<string> flags = new List<string>();
+        int index = 0;
 
-        // Split the input string into words
-        string[] words = input.Split(' ');
+        bool running = true;
 
-        // Loop through each word and identify flags
-        foreach (string word in words)
-        {
-            if (word.StartsWith("-"))
-            {
-                // If the flag contains a '(', remove the part after it
-                int index = word.IndexOf('(');
-                if (index != -1)
-                {
-                    flags.Add(word.Substring(0, index));
+        // Continue looping until all flags are found
+        while ((index < input.Length) && (running == true)){
+            
+            // Find the next flag
+            int flagStart = input.IndexOf('-', index);
+            if (flagStart == -1){
+                // No more flags found, exit the loop
+
+                running = false;
+                return flags;
+            }
+
+            else{
+                // Check if the flag is inside parentheses
+                bool insideParentheses = false;
+                for (int i = flagStart; i >= 0; i--){
+                    if (input[i] == '('){
+                        insideParentheses = true;
+                        i = -1;
+                    }
+                    else if (input[i] == ')'){
+                        insideParentheses = false;
+                        i = -1;
+                    }
                 }
-                else
-                {
-                    flags.Add(word);
+
+                if (insideParentheses == false){
+
+                    // Find the end of the flag
+
+                    // the end of the flag can ether be a ' ' or 
+                    // a ')'
+                    int nextOpen = input.IndexOf('(', flagStart);
+                    int nextClose = input.IndexOf(')', flagStart);
+                    int nextSpace = input.IndexOf(' ', flagStart);
+
+                    int flagEnd;
+                    
+                    // if the next '(' is closer than the next ' ',
+                    // then the flag is ended with a ')'
+                    if (nextOpen < nextSpace){
+                        flagEnd = nextClose;
+                    }
+                    
+                    // if not its ended with a space.
+                    else{
+                        flagEnd = nextSpace;
+                    }
+                    
+
+                    if ((flagEnd == -1)){
+                        
+                        // No ')' found, exit the loop
+                        print(
+                            "The command you input contains an unclosed parentheses.  " + 
+                            "Please try again."
+                        );
+                        running = true;
+                        return null;
+                    }
+
+                    else{
+                        // Check for nested parentheses
+                        int nestedStart = input.IndexOf('(', flagStart + 1);
+
+                        while (nestedStart != -1 && nestedStart < flagEnd){
+                            int nestedEnd = input.IndexOf(')', nestedStart);
+                            if (nestedEnd == -1){
+                                // No closing parenthesis found, exit the loop
+                                return null;
+                            }
+                            nestedStart = input.IndexOf('(', nestedStart + 1);
+                        }
+
+                        // Add the flag with parameter to the list
+                        flags.Add(input.Substring(flagStart, flagEnd - flagStart + 1));
+                        index = flagEnd + 1;
+                    }
+                }
+                else{
+                    // Flag is inside parentheses, skip it
+                    index = flagStart + 1;
                 }
             }
-        }
 
+        } // while index < input.Length) && (running == true)
+
+        // Return the list of flags
         return flags;
     }
 
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,7 +8,12 @@ using System.Text.Json;
 // flag classes, and add the function classes to the functionObjects list
 // on line 27
 
-class Program{
+/// <summary>
+/// a framework that get commands from users. To add a command create a 
+/// functionClass and add a new object to the function object list
+/// This code snippet was written with the help of ChatGPT.
+/// </summary>
+public static class Program{
     public static bool running;
     
     public static char[] bannedChars =" &%$#@*(){}[]'=+_".ToCharArray();
@@ -18,9 +23,12 @@ class Program{
 
     private static List<functionClass> functionObjects;
 
+    public static string Title = "Program.cs";
+
     static void Main(string[] args){
 
-        print($"\n== Welcome to Scriptures.cs ==\n");
+
+        print($"\n== Welcome to {Title} ==\n");
 
         running = true;
 
@@ -43,18 +51,23 @@ class Program{
         // keep the program running until the user tells it to stop.   
         while (running == true){
             // get input from the user to determin what to do.
-            string rawInput = input("scriptures.cs> ").ToLower();
+            string rawInput = input($"{Title}> ");
 
-            string[] splitInput = rawInput.Split(" ");
+            List<string> copyData = ExtractCommand(rawInput);
 
-            commandInput = splitInput[0];
+            commandInput = copyData[0];
 
-            flagInput = splitInput.Skip(1).ToArray().ToList();
+            flagInput = ExtractFlagsAndParameters(copyData[1]);
+
             
             
+            if (flagInput == null){
+                // do nothing, ExtractFlagsAndParameters handels informing 
+                // the user of errors.
+            }
             // if the user asked for help and the program is still running
             // run the help function
-            if (commandInput == "help" ){
+            else if (commandInput == "help" ){
                 help(functionObjects, flagInput);
             }
 
@@ -81,6 +94,151 @@ class Program{
 
     }
 
+    /// <summary>
+    /// extract the command from a string.
+    /// This code snippet was written with the help of ChatGPT.
+    /// </summary>
+    /// <param name="input">a string</param>
+    /// <returns>a list where the first index is the command and the second is the rest of the string</returns>
+    /// <author>This code snippet was written with the help of ChatGPT.</author>
+    private static List<string> ExtractCommand(string input){
+        List<string> output = new List<string>();
+        int firstSpace = input.IndexOf(' ');
+        if (firstSpace == -1){
+            // There is only one word in the input
+            output.Add(input);
+            output.Add("");
+        }
+        else{
+            output.Add(input.Substring(0, firstSpace));
+            output.Add(input.Substring(firstSpace + 1));
+        }
+        return output;
+    }
+    
+    /// <summary>
+    /// Extracts a flag and parameter as a single string from a given input string.
+    /// A flag is defined as a word that starts with the '-' character. 
+    /// The parameter is at the end of the flag and is surrounded by parentheses. 
+    /// </summary>
+    /// <param name="input">The input string to extract flags and parameters from</param>
+    /// <returns>A List of strings containing the flags and their corresponding parameters (if any)</returns>
+    /// <author>This code snippet was written with the help of ChatGPT.</author>
+    private static List<string> ExtractFlagsAndParameters(string input){
+        List<string> flags = new List<string>();
+        int index = 0;
+
+        bool running = true;
+
+        // Continue looping until all flags are found
+        while ((index < input.Length) && (running == true)){
+            
+            // Find the next flag
+            int flagStart = input.IndexOf('-', index);
+            if (flagStart == -1){
+                // No more flags found, exit the loop
+
+                running = false;
+                return flags;
+            }
+
+            else{
+                // Check if the flag is inside parentheses
+                bool insideParentheses = false;
+                for (int i = flagStart; i >= 0; i--){
+                    if (input[i] == '('){
+                        insideParentheses = true;
+                        i = -1;
+                    }
+                    else if (input[i] == ')'){
+                        insideParentheses = false;
+                        i = -1;
+                    }
+                }
+
+                if (insideParentheses == false){
+
+                    // Find the end of the flag
+
+                    // the end of the flag can ether be a ' ' or 
+                    // a ')'
+                    int nextOpen = input.IndexOf('(', flagStart);
+                    int nextClose = input.IndexOf(')', flagStart);
+                    int nextSpace = input.IndexOf(' ', flagStart);
+
+                    int flagEnd;
+                    
+                    // if the next '(' is closer than the next ' ',
+                    // then the flag is ended with a ')'
+                    if (nextOpen == -1){
+                        nextOpen = input.Length;
+                    }
+
+                    if (nextSpace == -1){
+                        nextSpace = input.Length;
+                    }
+
+                    // the only way next open and nextspace can be equal 
+                    // is if they are both set the the lenght of input.
+                    if (nextOpen  == nextSpace){
+                        flagEnd = input.Length - 1;
+                    }
+
+                    else if (nextOpen < nextSpace){
+                        flagEnd = nextClose;
+                    }
+                    
+                    // if not its ended with a space.
+                    else{
+                        flagEnd = nextSpace;
+                    }
+                    
+
+                    if ((flagEnd == -1)){
+                        
+                        // No ')' found, exit the loop
+                        print(
+                            "The command you input contains an unclosed parentheses.  " + 
+                            "Please try again."
+                        );
+                        running = true;
+                        return null;
+                    }
+
+                    else{
+                        // Check for nested parentheses
+                        int nestedStart = input.IndexOf('(', flagStart + 1);
+
+                        while (nestedStart != -1 && nestedStart < flagEnd){
+                            int nestedEnd = input.IndexOf(')', nestedStart);
+                            if (nestedEnd == -1){
+                                // No closing parenthesis found, exit the loop
+                                return null;
+                            }
+                            nestedStart = input.IndexOf('(', nestedStart + 1);
+                        }
+
+                        // Add the flag with parameter to the list
+                        flags.Add(input.Substring(flagStart, flagEnd - flagStart + 1));
+                        index = flagEnd + 1;
+                    }
+                }
+                else{
+                    // Flag is inside parentheses, skip it
+                    index = flagStart + 1;
+                }
+            }
+
+        } // while index < input.Length) && (running == true)
+
+        // Return the list of flags
+        return flags;
+    }
+
+    /// <summary>
+    /// construct a registry of fucntions that the framework can run
+    /// </summary>
+    /// <returns>a dictionary containing command and the funcitons they run</returns>
     private static void constructFramework(){
         // create a list of commands
         // "help" is preloaded into this to account for a help function
@@ -139,6 +297,12 @@ class Program{
         }
     }
 
+    /// <summary>
+    /// a command that displays a list of funcitons, the commands that run them
+    /// and any flags or peramiters
+    /// </summary>
+    /// <param name="functions">a list of funciton objects</param>
+    /// <param name="flags"> a list of flags the user input.</peram>
     private static void help(List<functionClass> functions, List<string> flags){
         
         if ((functions.Count != 0) && (flags.Count == 0)){
@@ -439,44 +603,12 @@ public abstract class functionClass{
 
                 string peram = "";
 
-                bool isValidPeram = true;
                 
                 // if the flag contains a ( assume the user is passing a paramiter and extract it.
-                if (flag.Contains('(')){
-                    if (flag.Contains(')') == false){
-                        Program.print($"flag '{flag}' contains a '(' but not ')'.  " +
-                        "Please try again.");
-                        isValidPeram = false;
-                    }
-                    else if (flag[flag.Length-1] != ')'){
-                        Program.print("The last char of a flag with a peramiter " + 
-                        "must be ')'.  Please try again.");
-                        isValidPeram = false;
-                    }
-                    else{
-
-                        // split the peramiter off the flag
-                        string[] splitFLag = flag.Split('(');
-                        
-                        // make sure there was only 1 ( char
-                        if (splitFLag.Count() != 2){
-                            Program.print("Flags with peramiters may only " + 
-                            "contain one '(' charicter.  Please try again.");
-                            isValidPeram = false;
-                        }
-
-                        else{
-                            flag = splitFLag[0];
-                            peram = splitFLag[1];
-
-                            // remvoe the ) from the end of peram
-                            peram = peram.Remove(peram.Length -1);
-                        }
-                        
-                    }
-                }
+                peram = ExtractParameter(flag);
                 
-                if (isValidPeram == true){
+                if (peram != null){
+                    flag = flag.Replace(("(" + peram + ")"), "");
                     try{
                         FlagRegistry[flag].run(peram);
                     }
@@ -486,16 +618,115 @@ public abstract class functionClass{
                     }
                 }
                 
-
-                
             }
-            else Program.print("No flags registered for this command.");
+            else {
+                Program.print("No flags registered for this command.");
+            }
             
         }
         else{
             Program.print("Only one flag is supported at this time.  Please try again.");
         }
         
+    }
+
+    /// <summary>
+    /// Extracts parameter from a string.
+    /// </summary>
+    /// <param name="input">A string</param>
+    /// <returns>A parameter (words surrounded with parentheses)</returns>
+    /// <author>This code snippet was written with the help of ChatGPT.</author>
+    public static string ExtractParameter(string input){
+        input = input + " ";
+        int openParentheses = 0;
+        int closeParentheses = 0;
+        string result = "";
+
+        // loop through every character in the input
+        for (int i = 0; i < input.Length; i++){
+            
+            char currentChar = input[i];
+
+            // if the current char is '('
+            if (currentChar == '('){
+
+                // If a nested parenthesis is detected,
+                // inform the user and return null
+                if (openParentheses > 0){
+                    Program.print(
+                        "The command you input includes nested opening parentheses. " +
+                        "Please try again."
+                    );
+                    return null;
+                }
+
+                // add one to the count of '('.
+                openParentheses++;
+            }
+
+            // if the current char is ')'
+            else if (currentChar == ')'){
+
+                // If a nested ')' is detected,
+                // inform the user and return null
+                if (closeParentheses > 0){
+                    Program.print(
+                        "The command you input includes nested closing parentheses.  " +
+                        "Please try again."
+                    );
+                    return null;
+                }
+                // if not, add the detected ')' to the count
+                else{
+                    closeParentheses++;
+
+                    // make sure tehre arnt multiple perams
+                    if (closeParentheses == 2){
+                        Program.print(
+                            "the command you input includes two peramiters attached to one flag.  " + 
+                            "Please try again."
+                        );
+                        return null;
+                    }
+                }
+
+                // if the number of '(' and ')' are the same, add what
+                // between them to the output string.
+                if (closeParentheses == openParentheses){
+                    
+                    // add one to the starting index to not include the '('
+                    int startIndex = input.LastIndexOf('(', i - 1) + 1;
+
+                    // subtract 1 form the length so the ')' is not included
+                    int length = i - startIndex;
+
+                    // extract the enclosed string
+                    result = input.Substring(startIndex, length);
+                }
+
+                // if there are more ')' than '(' inform the user and return null
+                else if (closeParentheses > openParentheses){
+                    Program.print(
+                        "The command you input has a closing parentheses " +
+                        "that comes before an opening parentheses. " +
+                        "Please try again."
+                    );
+                    return null;
+                }
+            }
+        }
+
+        // If there are any open or close parentheses remaining, inform the user and return null
+        if (openParentheses > 1 || closeParentheses > 1){
+            Program.print(
+                "The command you input has an unclosed parentheses. " +
+                "Please try again."
+            );
+            return null;
+        }
+
+        // Return the extracted parameter
+        return result;
     }
 
     protected abstract void runNoFlag();
